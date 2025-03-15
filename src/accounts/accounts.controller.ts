4ad6a3bb83770b4737b16fb0f591703c18abeb43
Eliane Isadora Faveron Maciel <eliane.faveron@gmail.com>
2025-03-15 14:11:07 +0000

    Adição de controller e services

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,33 @@
 import { Controller } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 
-@Controller('contas')
-export class AccountsController {}
+@Controller('accounts')
+export class AccountsController {
+  constructor(private readonly accountsService: AccountsService) {}
+
+  @Post()
+  create(@Body() conta: any) {
+    return this.accountsService.create(conta);
+  }
+
+  @Get()
+  findAll() {
+    return this.accountsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.accountsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() conta: any) {
+    return this.accountsService.update(id, conta);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.accountsService.remove(id);
+  }
+}
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+    constructor(
+    ) {}
+  
+    create(conta: any)  {
+        return "Objeto criado"
+    }
+  
+    findAll()  {
+        return "Objeto encontrado"
+    }
+  
+    findOne(id: number)  {
+        return "Objeto encontrado"
+    }
+  
+    async update(id: number, conta: any)  {
+        return "Objeto editado"
+    }
+  
+    async remove(id: number)  {
+        return "Objeto removido"
+    }
+}
