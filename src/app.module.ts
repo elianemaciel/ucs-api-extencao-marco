1ae5c5da059e0f42a40c82b8eafbf2224cb91d5c
Eliane Isadora Faveron Maciel <eliane.faveron@gmail.com>
2025-03-28 01:37:43 +0000

    Config envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AccountsModule, MongooseModule.forRoot('mongodb://localhost/')],
+  imports: [ConfigModule.forRoot(),
+    AccountsModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: 'test',
+      useFactory: async (config: ConfigService) => ({
+       uri: config.get('MONGODB_URL'),
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
